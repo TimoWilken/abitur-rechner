525707b132c10206ffd6221ab876b79bf7fbeac5
Timo Wilken <timo.21.wilken@gmail.com>
2018-01-14 17:16:04 +0000

    Add documentation on exam rules.

--- exams.js
+++ exams.js
@@ -1,3 +1,17 @@
+function unmetExamRequirements() {
+    /*
+     * RULES:
+     *
+     * - at least one exam subject must be one of Deutsch, Mathematik, and a
+     *   Fremd-/Landessprache taught to a high level
+     * - point count (after getSubjectTotalExamPoints) in at least 3 exam
+     *   subjects (incl. one of the above) must be >= 20
+     * - points count of written only (if written.enabled) else oral only (if
+     *   oral.enabled) exams must have a total for all 5 exam subjects together
+     *   of >= 25
+     */
+}
+
 function getSubjectTotalExamPoints(examGrades, extrapolatedGrade) {
     var writtenGrade = (examGrades.written.grade != undefined) ? examGrades.written.grade : extrapolatedGrade;
     var oralGrade = (examGrades.oral.grade != undefined) ? examGrades.oral.grade : extrapolatedGrade;
