d1b70c4d380d8e430b8361731fdaf762ed08d902
Timo Wilken <timo.21.wilken@gmail.com>
2018-05-17 15:13:04 +0100

    Reorder imports.

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 import styles from 'abirechner.css';
 
-import 'app.js';
+import 'util-functions.js';
+import 'util.js';
 import 'constants.js';
-import 'exams.js';
-import 'file.js';
-import 'grades.js';
-import 'requirements.js';
 import 'serializable.js';
+import 'watchable.js';
 import 'subjectconfig.js';
 import 'subject.js';
+import 'grades.js';
+import 'requirements.js';
+import 'exams.js';
+import 'file.js';
 import 'table.js';
 import 'terms.js';
 import 'ui.js';
-import 'util-functions.js';
-import 'util.js';
-import 'watchable.js';
+import 'app.js';
